2ca34cf654ea43993d3b18ee9c47b52675693698
Tino Didriksen <mail@tinodidriksen.com>
2014-08-07 09:56:19 +0000

    Dist OS X
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/opensource/packaging@10060 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- cg3/dist/dist-osx.pl
+++ cg3/dist/dist-osx.pl
@@ -11,20 +11,28 @@ print STDERR `make -j5`;
 my $revision = `svnversion -n`;
 $revision =~ s/^([0-9]+).*/$1/g;
 
-print STDERR `rm -rfv /tmp/vislcg3-0.9.8.$revision-osx* 2>&1`;
+print STDERR `rm -rfv /tmp/vislcg3-*-osx* 2>&1`;
 mkdir('/tmp/vislcg3-0.9.8.'.$revision.'-osx');
 chdir('/tmp/vislcg3-0.9.8.'.$revision.'-osx');
 mkdir('lib');
+mkdir('lib/cg3');
 mkdir('bin');
 print STDERR `cp -av $dname/../src/vislcg3 ./bin/ 2>&1`;
 print STDERR `cp -av $dname/../src/cg-comp ./bin/ 2>&1`;
 print STDERR `cp -av $dname/../src/cg-proc ./bin/ 2>&1`;
 print STDERR `cp -av $dname/../src/cg-conv ./bin/ 2>&1`;
+print STDERR `cp -av $dname/../src/libcg3.0.dylib ./lib/ 2>&1`;
+print STDERR `cp -av $dname/../src/libcg3-private.dylib ./lib/cg3/ 2>&1`;
 foreach my $bin (('vislcg3','cg-comp','cg-proc','cg-conv')) {
    print STDERR `ln -sv ./bin/$bin $bin`;
-   print STDERR `install_name_tool -change libicuuc.51.dylib \@executable_path/../lib/libicuuc.51.dylib ./bin/$bin`;
-   print STDERR `install_name_tool -change libicuio.51.dylib \@executable_path/../lib/libicuio.51.dylib ./bin/$bin`;
-   print STDERR `install_name_tool -change libicui18n.51.dylib \@executable_path/../lib/libicui18n.51.dylib ./bin/$bin`;
+   print STDERR `install_name_tool -change libicuuc.52.dylib \@executable_path/../lib/libicuuc.52.dylib ./bin/$bin`;
+   print STDERR `install_name_tool -change libicuio.52.dylib \@executable_path/../lib/libicuio.52.dylib ./bin/$bin`;
+   print STDERR `install_name_tool -change libicui18n.52.dylib \@executable_path/../lib/libicui18n.52.dylib ./bin/$bin`;
+}
+foreach my $lib (('cg3/libcg3-private.dylib','libcg3.0.dylib')) {
+   print STDERR `install_name_tool -change libicuuc.52.dylib \@executable_path/../lib/libicuuc.52.dylib ./lib/$lib`;
+   print STDERR `install_name_tool -change libicuio.52.dylib \@executable_path/../lib/libicuio.52.dylib ./lib/$lib`;
+   print STDERR `install_name_tool -change libicui18n.52.dylib \@executable_path/../lib/libicui18n.52.dylib ./lib/$lib`;
 }
 print STDERR `cp -av $dname/../scripts/cg3-autobin.pl ./bin/ 2>&1`;
 print STDERR `cp -av /usr/local/lib/libicu*.dylib ./lib/ 2>&1`;
