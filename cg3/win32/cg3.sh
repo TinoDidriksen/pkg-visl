47423357a1016c983c6ec628c11235d82c108752
Tino Didriksen <mail@tinodidriksen.com>
2015-02-24 13:58:36 +0000

    Rename DLLS
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/opensource/packaging@10554 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- cg3/win32/cg3.sh
+++ cg3/win32/cg3.sh
@@ -1,2 +1,2 @@
 ./cmake.sh -DCMAKE_TOOLCHAIN_FILE=/opt/mxe/usr/i686-w64-mingw32.shared/share/cmake/mxe-conf.cmake --prefix=/opt/win32
-export DLLS="icudt54.dll icuin54.dll icuio54.dll icuuc54.dll $DLLS"
+export EXTRA_DEPS="icudt54.dll icuin54.dll icuio54.dll icuuc54.dll $EXTRA_DEPS"
