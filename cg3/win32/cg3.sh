130e0a33b42464fcc135ceb9849c655aa334265c
Tino Didriksen <mail@tinodidriksen.com>
2015-10-15 16:08:48 +0000

    Autodep
    git-svn-id: svn+ssh://beta.visl.sdu.dk/usr/local/svn/repos/visl/opensource/packaging@11001 cb2587b3-b7ff-0310-8c81-a2a651690ada

--- cg3/win32/cg3.sh
+++ cg3/win32/cg3.sh
@@ -1,4 +1,3 @@
 ./cmake.sh -DCMAKE_TOOLCHAIN_FILE=/opt/mxe/usr/i686-w64-mingw32.shared/share/cmake/mxe-conf.cmake --prefix=/opt/win32
 mkdir -p /opt/win32-pkg/$PKG_NAME/opt/win32/bin/
 ln -s vislcg3.exe /opt/win32-pkg/$PKG_NAME/opt/win32/bin/cg3.exe
-export EXTRA_DEPS="icudt54.dll icuin54.dll icuio54.dll icuuc54.dll $EXTRA_DEPS"
--- cg3ide/win32/cg3ide.sh
+++ cg3ide/win32/cg3ide.sh
@@ -1,7 +1,6 @@
 install_dep cg3
 /opt/mxe/usr/i686-w64-mingw32.shared/qt5/bin/qmake cg3ide.pro PREFIX=/opt/win32
 QT5="../qt5/bin"
-export EXTRA_DEPS="icudt54.dll icuin54.dll icuio54.dll icuuc54.dll libharfbuzz-0.dll libpng16-16.dll libpcre16-0.dll libpcre-1.dll libiconv-2.dll libfreetype-6.dll libglib-2.0-0.dll libbz2.dll libintl-8.dll zlib1.dll $QT5/Qt5Core.dll $QT5/Qt5Gui.dll $QT5/Qt5Widgets.dll $EXTRA_DEPS"
 export EXTRA_INST="release/cg3ide.exe release/cg3processor.exe"
 
 mkdir -pv /opt/win32-pkg/$PKG_NAME/opt/win32/bin
